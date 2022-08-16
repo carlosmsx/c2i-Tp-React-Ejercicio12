6a3b0c93f562d091b94aa2d106dfbb02e78abba2
carlosmsx <carlosmsx@gmail.com>
2022-08-15 21:50:47 -0300

    agregada lógica para filtrar por pais

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,23 +4,38 @@ import { Form } from 'react-bootstrap';
 import Spinner from './Spinner'
 
 const Formulario = () => {
-    const apiKey = 'b73be4b9b9ff4a54b30c97e0f4f98766';
-    //https://newsapi.org/v2/top-headlines/sources?category=general&apiKey=b73be4b9b9ff4a54b30c97e0f4f98766
+    const apiKey = '0ee220cb193a4a54929b6e4dc46989b3'; //'b73be4b9b9ff4a54b30c97e0f4f98766';
+    
+    const paises = ['de','sa','ar','au','at','be','br','bg','ca','cn','co','cu','eg','sk','si','es','us','ph','fr','gr','nl','hk','hu','in','id','ie','il','it','jp','kr','lt','my','ma','mx','ng','no','nz','pl','pt','gb','cz','ro','ru','rs','sg','se','ch','th','tw','tr','ua','ve'];
+    const paisesNombres = ['Alemania','Arabia Saudita','Argentina','Australia','Austria','Bélgica','Brasil','Bulgaria','Canada','China','Colombia','Cuba','Egipto','Eslovakia','Eslovenia','España','Estados Unidos','Filipinas','Francia','Grecia','Holanda','Hong Kong','Hungría','India','Indonesia','Irlanda','Israel','Italia','Japon','Korea','Lituania','Malasia','Marruecos','Mejico','Nigeria','Noruega','Nueva Zelanda','Polonia','Portugal','Reino Unido','República Checa','Rumania','Rusia','Serbia','Singapur','Suecia','Suiza','Tailandia','Taiwan','Turquía','Ucrania','Venezuela'];
 
+    const [pais, setPais] = useState('todos');
+    const [categoria, setCategoria] = useState('');
     const [listaNoticias, setListaNoticias] = useState([]);
     const [mostrarSpinner, setMostrarSpinner] = useState(false);
 
-    const consultarAPI = async (category)=>{
+    const consultarAPI = async (category, country)=>{
+        if (categoria==='ninguna')
+            return;
+
         setMostrarSpinner(true);
-        const respuesta = await fetch(`https://newsapi.org/v2/top-headlines/sources?category=${category}&apiKey=${apiKey}`);
-        const dato = await respuesta.json();
-        setListaNoticias(dato.sources);
+        let url = (country==='todos')
+                ?`https://newsapi.org/v2/top-headlines/sources?category=${category}&apiKey=${apiKey}`
+                :`https://newsapi.org/v2/top-headlines/sources?category=${category}&country=${country}&apiKey=${apiKey}`
+        const respuesta = await fetch(url);
+        const lista = await respuesta.json();
+        setListaNoticias(lista.sources);
         setMostrarSpinner(false);
     }
 
-    const change = (event)=>{
-        if (event.target.value.length>0)
-            consultarAPI(event.target.value);
+    const categoryChange = async (event)=>{
+        setCategoria(event.target.value);
+        await consultarAPI(event.target.value, pais);
+    }
+
+    const countryChange = async (event)=>{
+        setPais(event.target.value);
+        await consultarAPI(categoria, event.target.value);
     }
 
     return (
@@ -28,8 +43,8 @@ const Formulario = () => {
             <Form>
                 <Form.Group className="mb-3 d-flex" controlId="formCategoria">
                     <Form.Label>Buscar por categoría</Form.Label>
-                    <Form.Select onChange={change}>
-                        <option value="">seleccione una categoría...</option>
+                    <Form.Select onChange={categoryChange}>
+                        <option value="ninguna">Seleccione una categoría...</option>
                         <option value="business">Negocios</option>
                         <option value="entertainment">Entretenimientos</option>
                         <option value="general">General</option>
@@ -37,6 +52,15 @@ const Formulario = () => {
                         <option value="science">Ciencia</option>
                         <option value="sports">Deportes</option>
                         <option value="technology">Tecnologia</option>
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3 d-flex" controlId="formCategoria">
+                    <Form.Label className="me-3">País</Form.Label>
+                    <Form.Select onChange={countryChange}>
+                        <option default value="todos">Todos</option>
+                        {
+                            paises.map((pais,posicion)=><option value={pais} key={posicion}>{paisesNombres[posicion]}</option>)            
+                        }
                     </Form.Select>
                 </Form.Group>
             </Form>
